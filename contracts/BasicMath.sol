7a5dea03c6fbd667cf98cec0dbe5f272b9924c1c
eGucio <egucio70@gmail.com>
2024-10-27 16:09:45 +0100

    done

---
+++ contracts/BasicMath.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract BasicMath {
+
+    function adder(uint _a, uint _b) external pure returns (uint sum, bool error) {
+        unchecked{
+            sum = _a + _b;
+            if(sum >= _a || sum >= _b){
+                return (sum, false);
+            } else{
+                return (0, true);
+            }
+
+        }
+    }
+
+    function subtractor(uint8 _a, uint8 _b) external pure returns (uint difference, bool error) {
+        unchecked{
+            difference = _a - _b;
+            if(difference <= _a){
+                return (difference, false);
+            } else{
+                return (0, true);
+            }
+
+        }
+    }
+}
